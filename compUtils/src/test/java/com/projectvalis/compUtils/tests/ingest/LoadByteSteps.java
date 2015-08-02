c88aad0605878bc921a2a647431087b10cae7395
projectvalis <davidholiday@hush.com>
2015-08-02 14:36:46 -0700

    all the stuff for a JBehave test is there, why isn't it running?

---
+++ compUtils/src/test/java/com/projectvalis/compUtils/tests/ingest/LoadByteSteps.java
@@ -0,0 +1,41 @@
+package com.projectvalis.compUtils.tests.ingest;
+
+import java.io.File;
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
+import com.projectvalis.compUtils.util.fileIO.Ingest;
+
+
+/**
+ * BDD tests for the ingest class
+ * @author funktapuss
+ *
+ */
+public class LoadByteSteps {
+
+	private String fNameS;
+	private byte[] byteARR;
+		
+	@Given("a file, $filename")
+	public void setFileName(String filename) {
+		fNameS = filename;
+	}
+	
+	@When("the caller loads the file as a byte array")
+	public void loadFile() {
+		byteARR = Ingest.loadFile(fNameS);
+	}
+	
+	@Then("the byte array that is returned contains the "
+			+ "correct number of bytes.") 
+	public void checkArrSize() {
+		File file = new File(fNameS);
+		assert (long)byteARR.length == file.length();
+	}
+
+	
+	
+}
---
+++ compUtils/src/test/resources/com/projectvalis/compUtils/tests/ingest/Ingest_stories.story
@@ -0,0 +1,12 @@
+compression utilities ingest story
+
+Narrative:
+In order to work with files to compress
+As a guy who wants to win a bet with cameron
+I want to ensures files are ingested and processed in the manner in which the
+methods in the ingest class purport to process them. 
+					 
+Scenario:  Simple test to give JBehave a test drive
+Given a file, a.log
+When the caller loads the file as a byte array
+Then the byte array that is returned contains the correct number of bytes. 

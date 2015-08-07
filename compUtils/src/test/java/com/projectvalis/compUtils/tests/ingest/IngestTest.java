d733dbcb8b15233baa500624edd57a4c063e2eb7
dholiday <dholiday@moesol.com>
2015-08-07 15:16:59 -0700

    finally got the reporting to work. why did the jbehave docs make this so difficult??

--- compUtils/src/test/java/com/projectvalis/compUtils/tests/ingest/IngestTest.java
+++ compUtils/src/test/java/com/projectvalis/compUtils/tests/ingest/IngestTest.java
@@ -3,14 +3,23 @@ package com.projectvalis.compUtils.tests.ingest;
 
 import org.jbehave.core.configuration.Configuration;
 import org.jbehave.core.configuration.MostUsefulConfiguration;
+import org.jbehave.core.embedder.StoryControls;
+import org.jbehave.core.io.CodeLocations;
+import org.jbehave.core.io.LoadFromClasspath;
+import org.jbehave.core.io.UnderscoredCamelCaseResolver;
 import org.jbehave.core.junit.JUnitStory;  
+import org.jbehave.core.parsers.RegexPrefixCapturingPatternParser;
+import org.jbehave.core.parsers.RegexStoryParser;
+import org.jbehave.core.reporters.Format;
+import org.jbehave.core.reporters.StoryReporter;
+import org.jbehave.core.reporters.StoryReporterBuilder;
 import org.jbehave.core.steps.InjectableStepsFactory;
 import org.jbehave.core.steps.InstanceStepsFactory;
 
 
 
 /**
- * not 100% on this yet - I *think* it's linking the stories to the step logic.
+ * finally got this working and reporting. the docs for this REALLY suck ass. 
  * @author funktapuss
  *
  */
@@ -18,7 +27,15 @@ public class IngestTest extends JUnitStory {
 	
     @Override 
     public Configuration configuration() { 
-    	return new MostUsefulConfiguration(); 
+    	return new MostUsefulConfiguration()            
+    			.useStoryLoader(
+    					new LoadFromClasspath(this.getClass().getClassLoader()))
+                .useStoryReporterBuilder(
+                        new StoryReporterBuilder()
+                            .withDefaultFormats()
+                            .withFormats(Format.HTML, Format.CONSOLE)
+                            .withRelativeDirectory("jbehave-report")
+                );
     }
 
     @Override
